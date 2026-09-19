b71a10789d5069660c08c2bc3940902099d55362
riskiekalimuda <riskie.kalimuda@gmail.com>
2026-09-19 10:39:45 +0700

    add clean code mass transit extension

---
+++ Extensions/MassTransitExtensions.cs
@@ -0,0 +1,46 @@
+﻿using MassTransit;
+using MessageMQCommon.MQ.Names;
+using MessageMQCommon.Parameters;
+using Purchase.Msv.Consumers;
+using Purchase.Msv.Models;
+
+namespace Purchase.Msv.Extensions
+{
+    public static class MassTransitExtensions
+    {
+        public static IServiceCollection AddCustomMassTransit(this IServiceCollection services, IConfiguration configuration)
+        {
+            var rabbitMQSetting = configuration.GetSection("RabbitMqSettings").Get<RabbitMQParameter>() ?? new RabbitMQParameter();
+
+            services.AddMassTransit(x =>
+            {
+                x.AddEntityFrameworkOutbox<PurchaseMsvDbContext>(o =>
+                {
+                    o.UsePostgres();
+                    o.UseBusOutbox();
+                    o.QueryDelay = TimeSpan.FromSeconds(10);
+                });
+
+                x.AddConsumersFromNamespaceContaining<PurchaseCreatedResultConsumer>();
+
+                x.UsingRabbitMq((context, cfg) =>
+                {
+                    cfg.Host(rabbitMQSetting.Host, rabbitMQSetting.VirtualHost, h =>
+                    {
+                        h.Username(rabbitMQSetting.Username);
+                        h.Password(rabbitMQSetting.Password);
+                    });
+
+                    cfg.ReceiveEndpoint(QueueNames.PurchaseQueue.PurchaseCreatedResultQueue, e =>
+                    {
+                        e.Durable = true;
+                        e.UseMessageRetry(r => r.Interval(20, 10));
+                        e.ConfigureConsumer<PurchaseCreatedResultConsumer>(context);
+                    });
+                });
+            });
+
+            return services;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -25,35 +25,7 @@ builder.Services.AddControllers();
 
 builder.Services.AddScoped<PurchaseService>();
 
-var rabbitMQSetting = builder.Configuration.GetSection("RabbitMqSettings").Get<RabbitMQParameter>()?? new RabbitMQParameter(); 
-
-builder.Services.AddMassTransit(x =>
-{
-    x.AddEntityFrameworkOutbox<PurchaseMsvDbContext>(o =>
-    {
-        o.UsePostgres();
-        o.UseBusOutbox();
-        o.QueryDelay = TimeSpan.FromSeconds(10);
-    });
-
-    x.AddConsumersFromNamespaceContaining<PurchaseCreatedResultConsumer>();
-
-    x.UsingRabbitMq((context, cfg) =>
-    {
-        cfg.Host(rabbitMQSetting.Host, rabbitMQSetting.VirtualHost, h =>
-        {
-            h.Username(rabbitMQSetting.Username);
-            h.Password(rabbitMQSetting.Password);
-        });
-
-        cfg.ReceiveEndpoint(QueueNames.PurchaseQueue.PurchaseCreatedResultQueue, e =>
-        {
-            e.Durable = true;
-            e.UseMessageRetry(r => r.Interval(20,10));
-            e.ConfigureConsumer<PurchaseCreatedResultConsumer>(context);
-        });
-    });
-});
+builder.Services.AddCustomMassTransit(builder.Configuration);
 
 var app = builder.Build();
 app.UseRouting();
